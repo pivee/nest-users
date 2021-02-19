e9d7be53c156d0522d9690d716d23c2d370e670d
Pivithuru Jayakody <piveedj@gmail.com>
2021-02-19 10:53:12 +0530

    Fix  Id in Users Module from Number to String

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -19,7 +19,7 @@ export class UsersController {
   }
 
   @MessagePattern('findOneUser')
-  findOne(@Payload() id: number) {
+  findOne(@Payload() id: string) {
     return this.usersService.findOne(id);
   }
 
@@ -29,7 +29,7 @@ export class UsersController {
   }
 
   @MessagePattern('removeUser')
-  remove(@Payload() id: number) {
+  remove(@Payload() id: string) {
     return this.usersService.remove(id);
   }
 }
